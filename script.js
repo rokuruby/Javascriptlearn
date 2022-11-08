3475b92fed5e78f81a6317b383867b47e3592acf
Tiong Lee <LLLTTH@gmail.com>
2022-11-09 03:28:55 +0800

    updated some test code for excercises

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+
+function blaBla() {
 for (let i = 0; i < 11; i++) {
-	
-	if (i = 0 || i = 1) {;}
-		else if ( i % 2 = 0) {
-		alert(i);
-		}
-	}
+	if (i == 0) {;}
+        else if ( i % 2 == 0) {
+	    alert(i);
+        }
+}
+}
+blaBla();
